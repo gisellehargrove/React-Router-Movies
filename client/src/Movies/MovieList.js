93e7b1acb56ad041c1cddc18b820f47c3c9a3b35
gisellehargrove <gisellehargrove@gmail.com>
2019-07-23 16:07:19 -0400

    completed MovieCard component

--- client/src/Movies/MovieList.js
+++ client/src/Movies/MovieList.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
+import MovieCard from './MovieCard';
 import axios from 'axios';
 
 const MovieList = props => {
@@ -22,33 +23,9 @@ const MovieList = props => {
   return (
     <div className="movie-list">
       {movies.map(movie => (
-        <MovieDetails key={movie.id} movie={movie} />
+        <MovieCard key={movie.id} movie={movie} />
       ))}
     </div>
-  );
-}
-
-function MovieDetails({ movie }) {
-  const { title, director, metascore, stars, id } = movie;
-  return (
-    <Link to={`/movies/${id}`}>
-      <div className="movie-card">
-        <h2>{title}</h2>
-        <div className="movie-director">
-          Director: <em>{director}</em>
-        </div>
-        <div className="movie-metascore">
-          Metascore: <strong>{metascore}</strong>
-        </div>
-        <h3>Actors</h3>
-
-        {stars.map(star => (
-          <div key={star} className="movie-star">
-            {star}
-          </div>
-        ))}
-      </div>
-    </Link>
   );
 }
 
